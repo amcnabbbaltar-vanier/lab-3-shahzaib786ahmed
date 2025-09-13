1b426b6f3417a0ca18d77c6a77a2441918a417fe
shahzaib786ahmed <ahmedshahzaib203@gmail.com>
2025-09-13 14:03:05 -0400

    Task 4 - Modified the Gun Component

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -12,20 +12,32 @@ public class GunComponent : MonoBehaviour
    void Update() 
    {
 
-    if (Input.GetButtonUp("Fire1"))
-    {
+    if (Input.GetButtonDown("Fire1")) {
+        isCharging = true;
+        chargeTime = 0f;
+    }
+    
+    if (Input.GetButton("Fire1") && isCharging) {
+        chargeTime += Time.deltaTime;
+        chargeTime = Mathf.Clamp(chargeTime, 0f, maxChargeTime);
+    }
+
+    if (Input.GetButtonUp("Fire1") && isCharging) {
         ShootBullet();
+        isCharging = false;
+        chargeTime = 0f;
     }
 
    }
 
    void ShootBullet()
-   {
-    GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-    Rigidbody rb = bullet.GetComponent<Rigidbody>();
+{
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+       
+        // Scale bullet force based on charge time
+        float bulletImpulse = (chargeTime / maxChargeTime) * bulletMaxImpulse;
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+    }
 
-    float bulletImpulse = bulletMaxImpulse;
-
-    rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.impulse);
-   }
 }
