7b011e25e17b7833e2cd6a63c4b2c0cca4241788
shahzaib786ahmed <ahmedshahzaib203@gmail.com>
2025-09-13 13:08:13 -0400

    Task 2 - Created a BulletComponent Script

---
+++ Assets/Script/BulletComponent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy object after a few seconds.
+        Destroy(gameObject, 5f);
+    }
+}
